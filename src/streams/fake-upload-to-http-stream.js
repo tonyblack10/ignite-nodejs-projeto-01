5dad1a8761d26fd7fb8960bde42ffff8419458c4
Tony Costa <tonyblack10@gmail.com>
2023-01-28 00:49:48 -0300

    aplicando Streams no modulo HTTP

---
+++ src/streams/fake-upload-to-http-stream.js
@@ -0,0 +1,26 @@
+import { Readable } from 'node:stream'
+
+class OneToHendredStream extends Readable {
+
+  index = 1
+
+  _read() {
+    const i = this.index++
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null)
+      } else {
+        const buffer = Buffer.from(String(i))
+
+        this.push(buffer)
+      }
+    }, 1000)
+  }
+
+}
+
+fetch('http://localhost:3334', {
+  method: 'POST',
+  body: new OneToHendredStream()
+})
