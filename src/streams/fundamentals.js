6a6c9104483e79f24b53dc549da7a096fff58d16
Tony Costa <tonyblack10@gmail.com>
2023-01-28 00:39:22 -0300

    Streams de escrita e transformacao

--- src/streams/fundamentals.js
+++ src/streams/fundamentals.js
@@ -1,4 +1,4 @@
-import { Readable } from 'node:stream'
+import { Readable, Writable, Transform } from 'node:stream'
 
 class OneToHendredStream extends Readable {
 
@@ -20,5 +20,21 @@ class OneToHendredStream extends Readable {
 
 }
 
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1
+
+    callback(null, Buffer.from(String(transformed)))
+  }
+}
+
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10)
+    callback()
+  }
+}
+
 new OneToHendredStream()
-  .pipe(process.stdout)
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream())
