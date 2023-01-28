fb0b809e45a48f28cc7c1d36b2705ce2ee1a10b9
Tony Costa <tonyblack10@gmail.com>
2023-01-28 00:09:34 -0300

    Salvando usuarios em memoria

--- src/server.js
+++ src/server.js
@@ -1,13 +1,23 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if (method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuários')
+    return res
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@email.com'
+    })
+
     return res.end('Criação de usuários')
   }
 
