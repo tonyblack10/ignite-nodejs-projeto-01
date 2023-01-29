e5aacd4be5e8e97e58b67b8cb1eef0e3a8eada30
Tony Costa <tonyblack10@gmail.com>
2023-01-29 17:53:09 -0300

    filtrando lista do banco de dados

--- src/routes.js
+++ src/routes.js
@@ -9,9 +9,12 @@ export const routes = [
     method: 'GET',
     path: buildRoutePath('/users'),
     handler: (req, res) => {
-      console.log(req.query)
+      const { search } = req.query
 
-      const users = database.select('users')
+      const users = database.select('users', {
+        name: search,
+        email: search
+      })
 
       return res.end(JSON.stringify(users))
     }
